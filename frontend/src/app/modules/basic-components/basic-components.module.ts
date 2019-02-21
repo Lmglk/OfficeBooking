b4767c685e434baa331ea8ed5581a74ffa8e65a2
Leonid Veretennikov <megalok10@gmail.com>
2019-02-22 01:46:47 +0300

    Created button component

--- frontend/src/app/modules/basic-components/basic-components.module.ts
+++ frontend/src/app/modules/basic-components/basic-components.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BlockComponent } from './components/block/block.component';
+import { ButtonComponent } from './components/button/button.component';
 
 @NgModule({
-    declarations: [BlockComponent],
+    declarations: [BlockComponent, ButtonComponent],
     imports: [CommonModule],
-    exports: [BlockComponent],
+    exports: [BlockComponent, ButtonComponent],
 })
 export class BasicComponentsModule {}
---
+++ frontend/src/app/modules/basic-components/components/button/button.component.ts
@@ -0,0 +1,21 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+
+@Component({
+    selector: 'ob-button',
+    templateUrl: './button.component.html',
+    styleUrls: ['./button.component.css'],
+})
+export class ButtonComponent {
+    @Input() name: string;
+    @Input() disabled: boolean;
+
+    @Output() click: EventEmitter<Event> = new EventEmitter();
+
+    public handleClick(event: Event) {
+        event.stopPropagation();
+
+        if (!this.disabled) {
+            this.click.emit(event);
+        }
+    }
+}
