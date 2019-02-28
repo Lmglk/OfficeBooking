ac5105f4d5f248916a0a4277364a308b1346d4c4
Leonid Veretennikov <megalok10@gmail.com>
2019-03-01 01:38:46 +0300

    removed unused methods

--- frontend/src/modules/icons/components/user-icon/user-icon.component.ts
+++ frontend/src/modules/icons/components/user-icon/user-icon.component.ts
@@ -1,12 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
     selector: 'ob-user-icon',
     templateUrl: './user-icon.component.html',
     styleUrls: ['./user-icon.component.css'],
 })
-export class UserIconComponent implements OnInit {
-    constructor() {}
-
-    ngOnInit() {}
-}
+export class UserIconComponent {}
