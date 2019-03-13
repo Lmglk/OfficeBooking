b2d14ff5f14b305e94112e98757330114072a6ef
Andrey Vedenin <vedandays@gmail.com>
2019-03-13 12:25:04 +0300

    Checks user's roles (spring security)

--- backend/src/main/java/com/vvt/officebooking/controller/place/PlaceController.java
+++ backend/src/main/java/com/vvt/officebooking/controller/place/PlaceController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import javax.transaction.Transactional;
@@ -29,6 +30,7 @@ public class PlaceController {
         return getItem(item);
     }
 
+    @PreAuthorize("hasAnyAuthority('ADMIN')")
     @PostMapping(value = "/save")
     @Transactional
     public ResponseEntity save(@RequestBody @Valid PlaceEntity item, @RequestParam @Valid Long idRoom) {
@@ -36,6 +38,7 @@ public class PlaceController {
         return getItem(newItem);
     }
 
+    @PreAuthorize("hasAnyAuthority('ADMIN')")
     @PostMapping(value = "/remove")
     @Transactional
     public void remove(@RequestBody @Valid PlaceEntity item) {
